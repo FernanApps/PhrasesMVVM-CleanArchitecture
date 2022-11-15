2045d77fc1db9581be913fb9939137789abd0752
FernanApp <fernan.app@gmail.com>
2022-11-15 14:32:23 -0500

    Init Commit : Simple MVVM

---
+++ app/src/main/java/practice/phrases/mvvm/view/MainActivity.kt
@@ -0,0 +1,38 @@
+package practice.phrases.mvvm.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import practice.phrases.mvvm.R
+import practice.phrases.mvvm.databinding.ActivityMainBinding
+import practice.phrases.mvvm.viewmodel.QuoteViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val quoteViewModel: QuoteViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initObserves()
+        initActions()
+        quoteViewModel.randomQuote()
+
+
+    }
+    private fun initObserves(){
+        quoteViewModel.quoteModel.observe(this, Observer { currentQuote ->
+            binding.tvQuote.text = currentQuote.quote
+            binding.tvAuthor.text = currentQuote.author
+        })
+
+    }
+    private fun initActions(){
+        binding.viewContainer.setOnClickListener { quoteViewModel.randomQuote() }
+
+    }
+}
